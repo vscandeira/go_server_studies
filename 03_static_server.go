f6fab5fc1cde3c28f86bb76da6443687202a6f2e
Victor Candeira <vscandeira@gmail.com>
2025-03-10 09:02:31 -0300

    Added basic log message

--- 03_static_server.go
+++ 03_static_server.go
@@ -4,11 +4,13 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./static")))
+	log.Print("Server Listening on port 9000")
 	err := http.ListenAndServe(":9000", nil)
 	if err != nil {
 		panic(err)
