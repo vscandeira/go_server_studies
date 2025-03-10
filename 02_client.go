57b5eb840b594d50c1b3ba0900b6f45e3bfdace7
Victor Candeira <vscandeira@gmail.com>
2025-03-10 08:38:15 -0300

    Added partial print of the body of response.

--- 02_client.go
+++ 02_client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net/http"
 )
@@ -15,5 +16,15 @@ func main() {
 	fmt.Printf("Response status: %s\n\n", string(resp.Status))
 	for field, content := range resp.Header {
 		fmt.Printf("%v: %v\n", field, content)
+	}
+
+	fmt.Println()
+
+	scanner := bufio.NewScanner(resp.Body)
+	for i := 0; scanner.Scan() && i < 5; i++ {
+		fmt.Println(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading response body")
 	}
 }
