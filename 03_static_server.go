ab04f69d38aa323e3c24fa6d54092d19354baaf8
Victor Candeira <vscandeira@gmail.com>
2025-03-10 08:52:20 -0300

    First server giving a static page

---
+++ 03_static_server.go
@@ -0,0 +1,13 @@
+//basic web server that serves file 03_static_page.html on directory static
+// through the root endpoint
+
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.ListenAndServe(":9000", nil)
+}
